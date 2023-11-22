0b0aeb59431e7be97c400b1add706bfa3e25b0d9
petrlarionov <petrlarionov@localhost>
2023-11-22 20:04:53 +0200

    Final appl code

--- java_advanced/src/telran/view/InputOutput.java
+++ java_advanced/src/telran/view/InputOutput.java
@@ -7,108 +7,136 @@ import java.util.function.Predicate;
 
 public interface InputOutput {
 	
-	String readString(String promt);
+	String readString(String prompt);
 	void write(String str);
 	
 	default void writeLine(Object obj) {
-		
+	
 		write(obj.toString() + "\n");
-		
+	
 	}
 	
-	default <T> T readObject(String promt, String errorPromt, Function<String, T> mapper) {
+	default <T> T readObject(String prompt, String errorPrompt,	Function<String, T> mapper) {
 		
 		boolean running = false;
 		T res = null;
 		
 		do {
-			
+		
 			running = false;
-			
+		
 			try {
-				
-				String str = readString(promt);
+			
+				String str = readString(prompt);
 				res = mapper.apply(str);
 				
 			} catch (Exception e) {
-				
 				running = true;
-				writeLine(errorPromt + ": " + e.getMessage());
-				
+				writeLine(errorPrompt + ": " + e.getMessage());
 			}
-			
+		
 		} while(running);
 		
 		return res;
-		
+	
 	}
 	
-	default int readInt(String promt, String errorPromt) {
-		
-		return readObject(promt, errorPromt, Integer::parseInt);
-		
+	default int readInt(String prompt, String errorPrompt) {
+	
+		return readObject(prompt, errorPrompt, Integer::parseInt);
+	
 	}
 	
-	default int readInt(String promt, String errorPromt, int min, int max) {
-		
-		return readObject(String.format("%s [%d-%d]", promt, min, max), errorPromt, str -> {
+	default int readInt(String prompt, String errorPrompt, int min, int max) {
+	
+		return readObject(String.format("%s [%d-%d]" , prompt, min, max), errorPrompt,
+				str -> {
 			int num = Integer.parseInt(str);
-			if (min < num || num > max) {
-				throw new RuntimeException(String.format("Must be in range [%d-%d]", min, max));
+			if (num < min || num > max) {
+				throw new RuntimeException
+				(String.format("must be in the range [%d-%d]", min, max));
 			}
 			return num;
 		});
 		
 	}
 	
-	default long readLong(String promt, String errorPromt) {
-		
-		return readObject(promt, errorPromt, Long::parseLong);
-		
+	default long readLong(String prompt, String errorPrompt) {
+	
+		return readObject(prompt, errorPrompt, Long::parseLong);
+	
 	}
 	
-	default long readLong(String promt, String errorPromt, long min, long max) {
+	default long readLong(String prompt, String errorPrompt, long min, long max) {
 		
-		return readObject(String.format("%s [%d-%d]", promt, min, max), errorPromt, str -> {
+		return readObject(String.format("%s [%d-%d]" , prompt, min, max), errorPrompt, str -> {
 			long num = Long.parseLong(str);
-			if (min < num || num > max) {
-				throw new RuntimeException(String.format("Must be in range [%d-%d]", min, max));
+			if (num < min || num > max) {
+				throw new RuntimeException
+				(String.format("must be in the range [%d-%d]", min, max));
 			}
 			return num;
 		});
-		
+	
 	}
 	
-	default LocalDate readDate(String promt, String errorPromt) { //example: 2023-11-20
+	default LocalDate readDate(String prompt, String errorPrompt) { //example: 2023-11-20
 		
-		return readObject(promt, errorPromt, LocalDate::parse);
+		return readObject(prompt, errorPrompt, LocalDate::parse);
+	
+	}
+	
+	default LocalDate readDate(String prompt, String errorPrompt, LocalDate from, LocalDate to) {
+		
+		return readObject(prompt, errorPrompt , str -> {
+			LocalDate date = LocalDate.parse(str);
+			if (date.compareTo(from) < 0 || date.compareTo(to) > 0) {
+				throw new RuntimeException(String.format("date must be in the range [%s - %s]",
+						from, to));
+			}
+			return date;
+		});
 		
 	}
 	
 	default String readPredicate(String prompt, String errorPrompt, Predicate<String> predicate) {
 		
-		//TODO
 		//returns string matching the given predicate
-		
-		return null;
+		return readObject(prompt, errorPrompt, str -> {
+			if (!predicate.test(str)) {
+				throw new RuntimeException("doesn't math predicate");
+			}
+			return str;
+		});
 		
 	}
 	
 	default String readOptions(String prompt, String errorPrompt, Set<String> options) {
-	
-		//TODO
+		
 		//returns string included in the given options
-	
-		return null;
-	
+		return readObject(prompt, errorPrompt, str -> {
+			if (!options.contains(str)) {
+				throw new RuntimeException("no option");
+			}
+			return str;
+		});
 	}
 	
 	default String readEmail(String prompt, String errorPrompt) {
-		
-		//TODO
 		//returns string with a email address
-		
-		return null;
+		return readObject(prompt, errorPrompt, str -> {
+			if(str.contains(" ")) {
+				throw new RuntimeException("cannot contain space");	
+			}
+			String[] emailParts = str.split("@");
+			if(emailParts.length != 2) {
+				throw new RuntimeException("must have two parts: name and domain separated by @");
+			}
+			if (!emailParts[1].contains(".")) {
+				throw new RuntimeException("domain must contain at least one dot");
+			}
+			return str;
+		});
 	
 	}
 	
@@ -122,14 +150,28 @@ public interface InputOutput {
 	
 			String errorPrompt, double min, double max) {
 	
-		return readObject(String.format("%s [%d-%d]" , prompt, min, max), errorPrompt, str -> {
-			double num = Double.parseDouble(str);
-			if (num < min || num > max) {
-				throw new RuntimeException
-				(String.format("must be in the range [%d-%d]", min, max));
+		return readObject(String.format("%s [%d-%d]" , prompt, min, max), 
+				errorPrompt, str -> {
+	
+					double num = Double.parseDouble(str);
+					if (num < min || num > max) {
+						throw new RuntimeException(String.format("must be in the range [%d-%d]", min, max));
 			}
 			return num;
 		});
 	
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	
 }
